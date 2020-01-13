8c6d15e93cb812861d986294f5dac95e8b6b0462
ContatoLucasSonntag <44034613+ContatoLucasSonntag@users.noreply.github.com>
2020-01-13 08:07:43 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,3 @@
-#!/bin/sh
-
-if [ ! -f "artisan" ]; then
-    echo "Could not find the Artisan file, Moving to Default Location."
-    cd /var/www/pterodactyl
-fi
-
-if [ ! -f "artisan" ]; then
-    echo "We tried to find your Artisan file but we couldnt, Please move to the directory you installed the Panel and re-run this script. Have a Good Day!"
-    cd /var/www/pterodactyl
-    else
-
-    echo "Your Artisan File has been found!"
-    sleep 2
-
-    echo "Checking you have ZIP Installed"
-    yum install zip -y 2> /dev/null
-    apt install zip -y 2> /dev/null
-
-    echo "Backing up previous panel files in the case that something goes wrong!"
-    zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
-
-    echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/ContatoLucasSonntag/PanelTheme.git .
-    cp -r v1#!/bin/sh
-
-if [ ! -f "artisan" ]; then
-    echo "Could not find the Artisan file, Moving to Default Location."
-    cd /var/www/pterodactyl
-fi
-
 if [ ! -f "artisan" ]; then
     echo "We tried to find your Artisan file but we couldnt, Please move to the directory you installed the Panel and re-run this script. Have a Good Day!"
     cd /var/www/pterodactyl
