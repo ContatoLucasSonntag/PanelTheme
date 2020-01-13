5e1733b12e0cefac1f414a7d734df149cf124375
ContatoLucasSonntag <44034613+ContatoLucasSonntag@users.noreply.github.com>
2020-01-13 09:14:36 -0300

    first223344

--- v1/build.sh
+++ v1/build.sh
@@ -22,8 +22,8 @@ if [ ! -f "artisan" ]; then
 
     echo "Downloading the Theme you picked"
     mkdir -p tempdown && cd tempdown && git clone https://github.com/ContatoLucasSonntag/PanelTheme.git .
-    cp -r PanelTheme/v1/public ..
-    cp -r PanelTheme/v1/resources ..
+    cp -r v1/public ..
+    cp -r v1/resources ..
 
     echo "Files have been copied over!"
     sleep 2
